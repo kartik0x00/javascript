f2d5a23df80cbfb5a7a1a4d6f52ff16716461fdc
kartikupadhyay <kartik.jagdish@outlook.com>
2025-06-13 22:25:23 +0530

    add summary

---
+++ summary/nullish-co.js
@@ -0,0 +1,18 @@
+// Nullish Coalescing Operator (??) : null, undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+val1 = undefined ?? 15
+
+console.log(val1);
+
+
+// Terniary Operator
+// condition ? true : false
+
+let iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("price is less than 80") : console.log("price is more than 80");
+
+//syntax--->    condition ? true : false
+
